46f23924e7f32f306c3f0c0fce6d557c22a1ea61
Daniel Fernandez <alejo.dafr@gmail.com>
2019-03-10 20:51:56 -0500

    Style sheet created

--- src/model/Pacman.java
+++ src/model/Pacman.java
@@ -4,5 +4,7 @@ import javafx.scene.shape.Arc;
 
 
 public class Pacman extends Arc {
-	
+	public Pacman(double cx, double cy, double rx, double ry, double ai, double l) {
+		super(cx, cy, rx, ry, ai, l);
+	}
 }
--- src/ui/PacmanController.java
+++ src/ui/PacmanController.java
@@ -1,16 +1,29 @@
 package ui;
 
+import javafx.event.ActionEvent;
+import javafx.event.Event;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.control.Menu;
 import javafx.scene.control.MenuBar;
 import javafx.scene.control.MenuItem;
 import javafx.scene.control.SeparatorMenuItem;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Arc;
+import javafx.scene.shape.ArcType;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.StrokeLineCap;
+import model.Pacman;
 
 public class PacmanController {
 
     @FXML
     private BorderPane window;
+    
+    @FXML
+    private Pane gameArea;
     
     @FXML
     public void initialize() {
@@ -20,6 +33,7 @@ public class PacmanController {
     	MenuItem load = new MenuItem("Load game");
     	MenuItem save = new MenuItem("Save game");
     	MenuItem exit = new MenuItem("Exit");
+    	
     	file.getItems().addAll(load, save, new SeparatorMenuItem(), exit);
     	
     	Menu view = new Menu("View");
@@ -27,8 +41,19 @@ public class PacmanController {
     	MenuItem instructions = new MenuItem("How to play");
     	view.getItems().addAll(leaderBoard, new SeparatorMenuItem(), instructions);
     	
-    	bar.getMenus().addAll(file, view);
+    	bar.getMenus().addAll(file, view);file.addEventHandler(ActionEvent.ACTION , new MenuItemSelected());
     	window.setTop(bar);
+    	Arc pac = new Pacman(0,0,31,31,45, 270);//pac.applyCss();
+    	pac.setLayoutX(400);
+    	pac.setLayoutY(400);//pac.setType(ArcType.ROUND);
+    	gameArea.getChildren().add(pac);
     }
 
+    public class MenuItemSelected implements EventHandler<ActionEvent> {
+
+		@Override
+		public void handle(ActionEvent event) {
+			// TODO Auto-generated method stub
+		}	
+    }
 }
